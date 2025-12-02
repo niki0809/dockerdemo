2a8bd9d4d47b193ad66453ecf98aa64f2acfefb8
niki0809 <jadhavnikitasuhas@gmail.com>
2025-12-02 18:31:36 +0530

    check flow

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
     // Apply to all requests
     const limiter = rateLimit({
         windowMs: 11 * 60 * 1000, // 15 minutes
-        max: 3, // Limit each IP to 100 requests per windowMs
+        max: 2, // Limit each IP to 100 requests per windowMs
         message: 'Too many requests from this IP, please try again after 15 minutes.'
     });
 
